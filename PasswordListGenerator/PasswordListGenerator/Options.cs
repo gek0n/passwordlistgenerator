f023187a4ad22c147f16c7c390b915e010a5c727
Михаил <gek0n@rambler.ru>
2016-07-27 22:54:11 +0500

    reformating files, add in/out encoding options

---
+++ PasswordListGenerator/PasswordListGenerator/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+
+namespace PasswordListGenerator
+{
+	public class Options
+	{
+		[Option('k', "keywords-file", Required = true, HelpText = "File with keywords for generate passwords")]
+		public string KeywordsFilename { get; set; }
+
+		[Option("in-encoding", DefaultValue = "UTF-8", Required = false, HelpText = "Define input files encoding")]
+		public string InFilesEncoding { get; set; }
+
+		[Option("out-encoding", DefaultValue = "UTF-8", Required = false, HelpText = "Define output files encoding")]
+		public string OutFilesEncoding { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			return null;
+		}
+	}
+}
--- PasswordListGenerator/PasswordListGenerator/Program.cs
+++ PasswordListGenerator/PasswordListGenerator/Program.cs
@@ -1,15 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using CommandLine.Text;
 
 namespace PasswordListGenerator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	internal class Program
+	{
+		private static Encoding _inFileEncoding;
+		private static Encoding _outFileEncoding;
+
+		private static void Main(string[] args)
+		{
+			var options = new Options();
+			if (CommandLine.Parser.Default.ParseArguments(args, options))
+			{
+				Console.WriteLine("Working with encodings:\nIN: {0}\nOUT: {1}\nFilename: {2}\n",
+					options.InFilesEncoding,
+					options.OutFilesEncoding,
+					options.KeywordsFilename);
+				
+				TrySetEncodings(options.InFilesEncoding, options.OutFilesEncoding);
+				using (var stream = new StreamReader(options.KeywordsFilename, _inFileEncoding))
+				{
+					while (!stream.EndOfStream)
+					{
+						var keyword = stream.ReadLine();
+						Console.OutputEncoding = _inFileEncoding;
+						Console.WriteLine(keyword);
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine(HelpText.AutoBuild(options));
+			}
+		}
+
+		private static void TrySetEncodings(string inEncoding, string outEncoding)
+		{
+			try
+			{
+				_inFileEncoding = Encoding.GetEncoding(inEncoding);
+			}
+			catch (ArgumentException)
+			{
+				Console.WriteLine("Error using {0} encoding. Fallback to utf-8", inEncoding);
+				_inFileEncoding = Encoding.UTF8;
+			}
+
+			try
+			{
+				_outFileEncoding = Encoding.GetEncoding(outEncoding);
+			}
+			catch (ArgumentException)
+			{
+				Console.WriteLine("Error using {0} encoding. Fallback to utf-8", outEncoding);
+				_outFileEncoding = Encoding.UTF8;
+			}
+			
+		}
+	}
 }
